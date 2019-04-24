3eaee00e9f5c256d61ffcb1a39efeda9b2099382
Wagura Maurice <wagura465@gmail.com>
2019-04-24 10:54:54 +0300

    drop down css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,6 +62,43 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+            /* The container <div> - needed to position the dropdown content */
+            .dropdown {
+              position: relative;
+              display: inline-block;
+            }
+
+            /* Dropdown Content (Hidden by Default) */
+            .dropdown-content {
+              display: none;
+              position: absolute;
+              background-color: #f9f9f9;
+              min-width: 160px;
+              box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+              z-index: 1;
+            }
+
+            /* Links inside the dropdown */
+            .dropdown-content a {
+              color: black;
+              padding: 12px 16px;
+              text-decoration: none;
+              display: block;
+            }
+
+            /* Change color of dropdown links on hover */
+            .dropdown-content a:hover {background-color: #f1f1f1}
+
+            /* Show the dropdown menu on hover */
+            .dropdown:hover .dropdown-content {
+              display: block;
+            }
+
+            /* Change the background color of the dropdown button when the dropdown content is shown */
+            .dropdown:hover .dropbtn {
+              background-color: #3e8e41;
+            }
         </style>
     </head>
     <body>
@@ -84,7 +121,13 @@
 
                 <div class="links">
                     <a href="{{ route('loans') }}">Loans Data</a>
-                    <a href="{{ route('maize') }}">Maize Reports</a>
+                    <div class="dropdown links">
+                      <a href="#">Maize Reports</a>
+                      <div class="dropdown-content links">
+                        <a href="{{ route('maize.report', 'planting') }}">Planting</a>
+                        <a href="{{ route('maize.report', 'harvesting') }}">Harvesting</a>
+                      </div>
+                    </div>
                 </div>
             </div>
         </div>
