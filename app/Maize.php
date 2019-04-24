85e80a4c7c60591b09000b17363e052911b9771f
Wagura Maurice <wagura465@gmail.com>
2019-04-24 10:56:52 +0300

    carbon dates on maize reports

--- app/Maize.php
+++ app/Maize.php
@@ -12,4 +12,58 @@ class Maize extends Model
 	{
 		return Maize::latest()->first();
 	}
+
+	public static function planting($data)
+	{
+		$i = 1;
+		foreach ($data as $key => $value) {
+
+		    $farmer = Farmers::getFarmer($value->id_number);
+
+		    if (isset($farmer->id)) {
+
+		        $insert[] = [
+		            'id' => Maize::latest_maize_report()->id + $i,
+		            'acres_planted' => $value->acreage,
+		            'kg_of_seed_planted' => $value->seeds_planted_kg,
+		            'farmer_id' => $farmer->id,
+		            'created_at' => \Carbon\Carbon::parse('2018-10-22 00:00:00')->addSeconds($i)->format('Y-m-d H:i:s'),
+		            'updated_at' => \Carbon\Carbon::parse('2019-04-22 00:00:00')->addSeconds($i)->format('Y-m-d H:i:s'),
+		            'report_type' => 'planting',
+		            'season' => $value->season,
+		            'kg_of_fertilizer' => $value->fertilizer_kg,
+		            'status' => 'pending'
+		        ];
+		    }
+		    $i++;
+		}
+
+		return $insert;
+	}
+
+	public static function harvesting($data)
+	{
+		$i = 1;
+		foreach ($data as $key => $value) {
+
+		    $farmer = Farmers::getFarmer($value->id_number);
+
+		    if (isset($farmer->id)) {
+
+		        $insert[] = [
+		            'id' => Maize::latest_maize_report()->id + $i,
+		            'farmer_id' => $farmer->id,
+		            'created_at' => \Carbon\Carbon::parse('2018-10-22 00:00:00')->addSeconds($i)->format('Y-m-d H:i:s'),
+		            'updated_at' => \Carbon\Carbon::parse('2019-04-22 00:00:00')->addSeconds($i)->format('Y-m-d H:i:s'),
+		            'bags_harvested' => $value->bags_harvested,
+		            'report_type' => 'harvest',
+		            'season' => $value->season,
+		            'status' => 'pending'
+		        ];
+		    }
+		    $i++;
+		}
+
+		return $insert;
+	}
 }
