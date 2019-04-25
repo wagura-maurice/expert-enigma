f81c304a1b8eed21255a5da4a165afb91f0b043b
Wagura Maurice <wagura465@gmail.com>
2019-04-25 08:52:03 +0300

    optimization on farmers

--- app/Farmers.php
+++ app/Farmers.php
@@ -8,23 +8,8 @@ class Farmers extends Model
 {
     protected $table= 'farmers';
 
-	/*protected $fillable = [
-		'title',
-		'description'
-	];
-
-	public static $create_rules = [
-		'title' => 'required|string|unique:sp_about_us',
-		'description' => 'required|string',
-	];
-
-	public static $update_rules = [
-		'title' => 'required|string',
-		'description' => 'required|string',
-	];*/
-
 	public static function getFarmer($value)
 	{
-		return Farmers::where(['national_id_number' => $value])->first();
+		return Self::where(['national_id_number' => $value])->first();
 	}
 }
