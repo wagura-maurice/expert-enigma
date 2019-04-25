9916169da750b341bacac3ba108ba9b018360b33
Wagura Maurice <wagura465@gmail.com>
2019-04-25 08:52:36 +0300

    optimization on loans ctrl

--- app/Http/Controllers/LoansController.php
+++ app/Http/Controllers/LoansController.php
@@ -41,7 +41,7 @@ class LoansController extends Controller
     {
         //validate the xls file
         $this->validate($request, array(
-            'file'      => 'required'
+            'file' => 'required'
         ));
 
         if($request->hasFile('file')){
@@ -52,42 +52,8 @@ class LoansController extends Controller
                 $data = Excel::load($path, function($reader) {
                 })->get();
                 if(!empty($data) && $data->count()) {
-                    $i = 1;
-                    foreach ($data as $key => $value) {
-
-                        $farmer = Farmers::getFarmer($value->id_number);
-
-                        if (isset($farmer->id)) {
-
-                            $insert[] = [
-                                'id' => Loans::latest_loan_report()->id + $i,
-                                'commodity' => 'inputs',
-                                'value' => $value->total_loan_amount,
-                                'time_period' => 'before planting',
-                                'season' => 3,
-                                'interest_rate' => 2.5,
-                                'interest_period' => 'month',
-                                'interest_type' => 'simple',
-                                'duration' => 6,
-                                'duration_unit' => 'month',
-                                'currency' => 'KES',
-                                'service_charge' => 0,
-                                'structure' => 'installments',
-                                'status' => 'application started',
-                                'disbursed_date' => '2018-10-22 00:00:00',
-                                'repaid_date' => '2019-04-22 00:00:00',
-                                'disbursal_method' => 'supplier',
-                                'repayment_method' => 'mpesa',
-                                'voucher_code' => strtoupper(Loans::generate_string(10)),
-                                'farmer_id' => $farmer->id,
-                                'created_at' => \Carbon\Carbon::parse(now())->addSeconds($i)->format('Y-m-d H:i:s'),
-                                'updated_at' => \Carbon\Carbon::parse(now())->addSeconds($i)->format('Y-m-d H:i:s'),
-                                'amount_paid' => $value->amount_paid,
-                                'service_charge_percentage' => 0
-                            ];
-                        }
-                        $i++;
-                    }
+                    
+                    $insert = Loans::proccess_loans($data);
 
                     if(!empty($insert)){
 
